167678905a2ea1c450c90cd5b9604fbdbb08cd56
Aravind-D <81469078+Aravind-D@users.noreply.github.com>
2021-08-08 17:25:45 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("REVBAILA", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+fill("white")
+    textSize(15)
+    text("CUTNIFON", 650,200)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 650,220)
+
+fill("white")
+textSize(15)
+text("ATADASBA", 200,350)
+fill("lightblue")
+text("Hint: Store all information !!", 200,370)
+}
